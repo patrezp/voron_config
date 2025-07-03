de7eafbbc585fd97c1a5aa7931867a5319388f01
patrezp <patrezp@gmail.com>
2025-07-03 22:30:05 +0200

    Autocommit from 2025-07-03 22:30:05
    Klipper on commit: 9346ad1
    
    Moonraker on commit: ccbfd64
    
    Mainsail version: v2.14.0

--- autocommit.sh
+++ autocommit.sh
@@ -32,7 +32,7 @@ mainsail_folder=~/mainsail
 
 ### The branch of the repository that you want to save your config
 ### By default that is 'master'
-branch=dragonburner_nfcrazy_g2sa
+branch=a4t_nfcrazy_g2sa_stealthchanger
 
 #####################################################################
 #####################################################################
